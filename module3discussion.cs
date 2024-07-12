e55dd10bd368eaadacd68380e42caa5fd62ba082
Pamella <cossapv@my.palmbeachstate.edu>
2024-07-11 22:22:03 -0400

    update

--- module3discussion.cs
+++ module3discussion.cs
@@ -1,2 +1,36 @@
+using System;
 
-Console.WriteLine("Hello, world!");
+
+public enum FurColor
+{
+    white,brown,black,Grey,Orange
+}
+public class Bunnyexample
+{
+    public string Name;
+    public bool LikesCarrots, LikesHUmans;
+    public FurColor color;
+
+    public Bunnyexample(){}
+    public Bunnyexample(string n) => Name = n;
+
+  public Bunnyexample (string name,bool likesCarrots = false,bool likesHumans = false)
+  {
+    Name = name;
+    LikesCarrots = likesCarrots;
+    LikesHumans = likesHumans; 
+  }
+
+    public static void Main(string[] args)
+    {
+        Bunnyexample b1 = new Bunnyexample { Name="Ed", LikesCarrots=true, LikesHumans=true };
+
+        Console.WriteLine($"Bunny 1: Name = {b1.Name}, Likes Carrots = {b1.LikesCarrots}, Likes Humans = {b1.LikesHumans}");
+
+        Console.ReadKey();
+    }
+}
+
+ 
+
+
--- obj/Debug/net8.0/COP2360.AssemblyInfo.cs
+++ obj/Debug/net8.0/COP2360.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("COP2360")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd44e3c5aacd8171cb700d8ba50495f68cb5ebcd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a641fc9d08a7199576ad4b3fafa14209ba788843")]
 [assembly: System.Reflection.AssemblyProductAttribute("COP2360")]
 [assembly: System.Reflection.AssemblyTitleAttribute("COP2360")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
