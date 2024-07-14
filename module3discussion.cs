6c6cc2fef8ba5258e3b188219f251132cc265ab4
Pamella <cossapv@my.palmbeachstate.edu>
2024-07-13 20:19:19 -0400

    update

--- module3discussion.cs
+++ module3discussion.cs
@@ -1,31 +1,34 @@
 using System;
 
-
 public enum FurColor
 {
-    white,brown,black,Grey,Orange
+    White,Brown,Black,Grey,Orange
 }
-public class Bunnyexample
+public class Bunny
 {
     public string Name;
-    public bool LikesCarrots, LikesHUmans;
-    public FurColor color;
+    public bool LikesCarrots, LikesHumans;
+    public FurColor Color;
 
-    public Bunnyexample(){}
-    public Bunnyexample(string n) => Name = n;
+    public Bunny() {}
+    public Bunny(string n) => Name = n;
 
-  public Bunnyexample (string name,bool likesCarrots = false,bool likesHumans = false)
+  public Bunny (string name,bool likesCarrots = false,bool likesHumans = false, FurColor color = FurColor.White)
   {
     Name = name;
     LikesCarrots = likesCarrots;
     LikesHumans = likesHumans; 
+    Color = color;
   }
+
 
     public static void Main(string[] args)
     {
-        Bunnyexample b1 = new Bunnyexample { Name="Ed", LikesCarrots=true, LikesHumans=true };
+        Bunny b1 = new Bunny { Name="Ed", LikesCarrots=true, LikesHumans=true, Color=FurColor.Grey};
+        Bunny b2 = new Bunny { Name="Bo", LikesCarrots=true, LikesHumans=false, Color=FurColor.Orange};
 
-        Console.WriteLine($"Bunny 1: Name = {b1.Name}, Likes Carrots = {b1.LikesCarrots}, Likes Humans = {b1.LikesHumans}");
+        Console.WriteLine($"Bunny 1: Name = {b1.Name}, Likes Carrots = {b1.LikesCarrots}, Likes Humans = {b1.LikesHumans}, Color = {b1.Color}");
+        Console.WriteLine($"Bunny 2: Name = {b2.Name}, Likes Carrots = {b1.LikesCarrots}, Likes HUmans = {b2.LikesHumans}, Color = {b2.Color}");
 
         Console.ReadKey();
     }
