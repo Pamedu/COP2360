d46e36f85ed81cb385044acb74025ee0d8654f7b
Pamella <cossapv@my.palmbeachstate.edu>
2024-07-18 21:17:56 -0400

    update

--- obj/Debug/net8.0/COP2360.AssemblyInfo.cs
+++ obj/Debug/net8.0/COP2360.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("COP2360")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7208954a070e5397cdbb5585f561fe102c5cbc96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1e9ec237e7165215eeb84791d82ecf38f53d55e")]
 [assembly: System.Reflection.AssemblyProductAttribute("COP2360")]
 [assembly: System.Reflection.AssemblyTitleAttribute("COP2360")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
